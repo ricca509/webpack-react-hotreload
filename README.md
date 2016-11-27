3d3cef9558ae955f1c343745daffe0d3c7a347b8
Riccardo Coppola <ricca509@gmail.com>
2016-11-27 15:09:04 +0000

    Makes the app smaller

---
+++ README.md
@@ -0,0 +1,10 @@
+# Webpack React hot reload config
+
+Configuration for Webpack 1.x
+
+Uses:
+ - webpack 1.x
+ - react
+ - Babel
+ - webpack-dev-middleware
+ - webpack-hot-middleware
--- app.js
+++ app.js
@@ -13,23 +13,22 @@ var config = require ('./webpack.dev.config');
 
 var compiler = webpack(config);
 
-var index = require('./routes/index');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Renders the index page
+app.use('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
