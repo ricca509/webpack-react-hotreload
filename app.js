423150ea3ccaeabfe4bf73689598d1ad0a6e0a7f
Riccardo Coppola <ricca509@gmail.com>
2016-11-27 19:34:23 +0000

    Adds CSS back

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Renders the index page
-app.use('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
